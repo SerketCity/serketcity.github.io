2560ebafd137d6262ce23d08e23fda0fb78ae203
daedalCacoethes <johnrice1997@gmail.com>
2026-01-12 05:58:52 -0500

    mild internal structuring + temp post

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,23 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 export default async function(eleventyConfig) {
     eleventyConfig.setOutputDirectory("docs");
+
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "atom",
+        outputPath: "/feed.xml",
+        collection: {
+            name: "post",
+            limit: 0
+        },
+        metadata: {
+            language: "en",
+            title: "egregore.garden",
+            subtitle: "a blog from me to you",
+            base: "https://egregore.garden/",
+            author: {
+                name: "john"
+            }
+        }
+    })
 };
